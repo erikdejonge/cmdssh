3515d47b54a08bda8c7198f54c725837f71a1bcb
Erik de Jonge <erik@a8.nl>
2015-03-23 22:11:36 +0100

    pip Monday 23 March 2015 (week:12 day:82), 22:11:33

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='cmdssh',
-      version='6',
+      version='7',
       description='Execute commands on local machine and on remote machine via ssh, and a wrapper for paramikos scp.',
       url='https://github.com/erikdejonge/cmdssh',
       author='Erik de Jonge',
