e32cf66879a3d7c890c2843d860e60515a73c46c
Erik de Jonge <erik@a8.nl>
2015-05-20 12:52:39 +0200

    pip Wednesday 20 May 2015 (week:20 day:140), 12:52:36

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+    # coding=utf-8
 """
 Profiler utility for python
 Active8 (04-03-15)
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='cmdssh',
-      version='27',
+      version='29',
       description='Execute commands on local machine and on remote machine via ssh, and a wrapper for paramikos scp.',
       url='https://github.com/erikdejonge/cmdssh',
       author='Erik de Jonge',
@@ -15,7 +15,7 @@ setup(name='cmdssh',
       license='GPL',
       packages=['cmdssh'],
       zip_safe=True,
-      install_requires=['requests', 'paramiko', 'consoleprinter', 'arguments', 'github', 'pytz'],
+      install_requires=['requests', 'paramiko', 'consoleprinter', 'arguments', 'pytz'],
       classifiers=[
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
